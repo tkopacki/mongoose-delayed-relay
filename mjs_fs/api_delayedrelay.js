f3a7b74748993ecd816f9a46f75924a2f34fde40
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-28 23:32:30 +0200

    .

--- mjs_fs/api_delayedrelay.js
+++ mjs_fs/api_delayedrelay.js
@@ -9,20 +9,15 @@ let startCounting = false;
 
 function init() {
     GPIO.set_mode(Cfg.get('relay.pin'), GPIO.MODE_OUTPUT);
-    GPIO.set_mode(1, GPIO.MODE_OUTPUT);
     GPIO.write(Cfg.get('relay.pin'), Cfg.get('relay.stateOff'));
     MQTT.sub(Cfg.get('relay.topic'), function (connection, topic, message) {
         if (message === "0") {
-            GPIO.write(1, 0);
             print('OFF signal recieved, starting counter...');
-            MQTT.pub('home/bathroom/callback', 'OFF signal recieved, starting counter...');
             startCounting = true;
             counter = 0;
         } else {
-            GPIO.write(1, 1);
             GPIO.write(Cfg.get('relay.pin'), Cfg.get('relay.stateOn'));
             print('Relay set to ON');
-            MQTT.pub('home/bathroom/callback', 'Relay set to ON');
             startCounting = false;
         }
     }, null);
@@ -34,14 +29,12 @@ function setTimer() {
         if (startCounting) {
             if (counter >= delay) {
                 print('OFF signal confirmed');
-                MQTT.pub('home/bathroom/callback', 'OFF signal confirmed');
                 GPIO.write(Cfg.get('relay.pin'), Cfg.get('relay.stateOff'));
                 startCounting = false;
                 counter = 0;
             } else {
                 counter++;
                 print('Still below treshold...');
-                MQTT.pub('home/bathroom/callback', 'Still below treshold...');
             }
         }
     }, null);
